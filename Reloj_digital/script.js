1008d30c53cee1af0e5c2a162e93e9bd9a34a16e
ReyY777 <139787117+ReyY777@users.noreply.github.com>
2023-10-17 21:29:41 -0500

    Add files via upload

---
+++ Reloj_digital/script.js
@@ -0,0 +1,17 @@
+function CalcularTiempo(){
+    let tiempo = new Date();
+
+    let hora = tiempo.getHours();
+    let minuto = tiempo.getMinutes();
+    let segundo = tiempo.getSeconds();
+
+    hora = hora <10 ? "0" + hora: hora;
+    minuto = minuto <10 ? "0" + minuto: minuto;
+    segundo = segundo <10 ? "0" + segundo: segundo;
+
+    let pantallaReloj = hora - 12 + ":" + minuto + ":" + segundo;
+    let relojDigital = document.querySelector(".reloj");
+    relojDigital.innerHTML = pantallaReloj;
+}
+
+setInterval(CalcularTiempo,1000);
